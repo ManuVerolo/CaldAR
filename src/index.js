6563d7c4a241a5d238a9c774c8983063df9d0f45
CesarPagliarini <capagliarini@gmail.com>
2021-10-05 14:58:02 -0300

    Cambio de puerto para Heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const router = require('./routes');
 const mongoose = require('mongoose');
 
 
-const port = 3000
+//const port = 3000
+const port = process.env.port || 5000;
 const app = express()
 
 app.use(express.json());            // Permite obtener el cuerpo del POST atravez del req.body
